7198e592615d556a2c130721971d296b090059c0
Showndarya <shmadhav@bu.edu>
2023-03-20 11:27:49 -0400

    Complete base script, add example tar file and tex file

--- scraping-data.py
+++ scraping-data.py
@@ -1,8 +1,10 @@
 import requests
 import xml.etree.ElementTree as ET
 import urllib.request
+import os
+import tarfile
 
-N = 10
+N = 5
 
 url = 'http://export.arxiv.org/api/query'
 
@@ -24,6 +26,38 @@ for entry in root.findall('{http://www.w3.org/2005/Atom}entry'):
 
 for arxiv_id in arxiv_ids:
     url = 'https://arxiv.org/e-print/' + arxiv_id
-    filename = 'archives/'+arxiv_id + '.tar.gz'
+    filename = arxiv_id + '.tar.gz'
     urllib.request.urlretrieve(url, filename)
     print('Downloaded ' + filename)
+
+for filename in os.listdir('.'):
+    print(filename)
+    if filename.endswith('.tar.gz'):
+        # Extract the .tar.gz file
+        with tarfile.open(filename) as tar:
+            # Find the .tex file in the archive
+            tex_file = None
+            for member in tar.getmembers():
+                if member.name.endswith('.tex'):
+                    tex_file = member
+                    break
+            # If a .tex file is found, extract the tikzpicture block
+            if tex_file is not None:
+                with tar.extractfile(tex_file) as tex:
+                    # Read the .tex file and find the tikzpicture block
+                    tikz = ''
+                    output_filename = filename.replace('.tar.gz', '.tex')
+                    in_tikz = False
+                    for line in tex:
+                        line = line.decode('utf-8')
+                        if '\\begin{tikzpicture}' in line:
+                            in_tikz = True
+                        if in_tikz:
+                            tikz += line
+                        if '\\end{tikzpicture}' in line:
+                            in_tikz = False
+                    with open(output_filename, 'w') as output:
+                        output.write(tikz)
+                    print('Extracted TikZ code from ' + filename + ' to ' + output_filename)
+            else:
+                print('No .tex file found in ' + filename)        
