73608029edd1e2a19bcde5935095674a28647f5b
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-03-22 13:52:26 -0400

    loop over all tex files

--- latex_compiler.py
+++ latex_compiler.py
@@ -9,14 +9,19 @@ from pathlib import Path
 import subprocess
 from sys import platform
 
+INPUT_DIRECTORY = Path("./output_tex")
 OUTPUT_DIRECTORY = Path("./output_pdf")
 
 if str(OUTPUT_DIRECTORY) not in os.listdir("."):
     os.mkdir(OUTPUT_DIRECTORY)
 
-command = ['pdflatex', '-output-directory', str(OUTPUT_DIRECTORY), 'output_tex/2303.11999v1_2.tex']
+# command = ['wsl', 'pdflatex', '-output-directory', str(OUTPUT_DIRECTORY), 'output_tex/2303.11999v1_0.tex']
+# subprocess.check_call(command)
 
+command = ['pdflatex', '-output-directory', str(OUTPUT_DIRECTORY)]
 if platform == "win32":
     command.insert(0, 'wsl')
 
-subprocess.check_call(command)
+# TODO : error handling, AFTER fixing inputs in scraping_data
+for filename in os.listdir(INPUT_DIRECTORY):
+    subprocess.check_call(command + [str(INPUT_DIRECTORY) + "/" + filename])
