94d77340845d49a21972990b9a5f6430aaee231a
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-03-22 12:50:57 -0400

    improved compiler

--- latex_compiler.py
+++ latex_compiler.py
@@ -4,10 +4,19 @@
 # If you want everything:
 # sudo apt-get install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
 
+import os
+from pathlib import Path
 import subprocess
 from sys import platform
 
+OUTPUT_DIRECTORY = Path("./output_pdf")
+
+if str(OUTPUT_DIRECTORY) not in os.listdir("."):
+    os.mkdir(OUTPUT_DIRECTORY)
+
+command = ['pdflatex', '-output-directory', str(OUTPUT_DIRECTORY), 'output_tex/2303.11999v1_2.tex']
+
 if platform == "win32":
-    subprocess.check_call(['wsl', 'pdflatex', 'output_tex/2303.11999v1_0.tex'])
-else:
-    subprocess.check_call(['pdflatex'])
+    command.insert(0, 'wsl')
+
+subprocess.check_call(command)
