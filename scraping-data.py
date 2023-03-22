2a3acee435bc53f1e2a615ae7caf3b68537888a2
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-03-22 00:27:01 -0400

    added error handlind and improved file logic

--- scraping-data.py
+++ scraping-data.py
@@ -1,10 +1,13 @@
+from pathlib import Path
 import requests
 import xml.etree.ElementTree as ET
 import urllib.request
 import os
 import tarfile
 
-N = 1
+NUMBER_OF_PAPERS = 10
+SOURCE_DIRECTORY = Path("./zipped_source")
+OUTPUT_DIRECTORY = Path("./output_tex")
 
 # arXiv API for getting the math/physics paper ids
 url = 'http://export.arxiv.org/api/query'
@@ -12,7 +15,7 @@ url = 'http://export.arxiv.org/api/query'
 params = {
     'search_query': 'cat:math* OR cat:physics*',
     'start': '0',
-    'max_results': str(N),
+    'max_results': str(NUMBER_OF_PAPERS),
     'sortBy': 'submittedDate',
     'sortOrder': 'descending'
 }
@@ -29,38 +32,52 @@ for entry in root.findall('{http://www.w3.org/2005/Atom}entry'):
 for arxiv_id in arxiv_ids:
     url = 'https://arxiv.org/e-print/' + arxiv_id
     filename = arxiv_id + '.tar.gz'
-    urllib.request.urlretrieve(url, filename)
-    print('Downloaded ' + filename)
+    if filename not in os.listdir(SOURCE_DIRECTORY):
+        urllib.request.urlretrieve(url, SOURCE_DIRECTORY / filename)
+        print('Downloaded ' + filename)
+    else:
+        print('Found ' + filename)
 
-for filename in os.listdir('.'):
-    print(filename)
-    # TODO: error handling when .tar.gz file is not formatted correctly
+print()
+
+for filename in os.listdir(SOURCE_DIRECTORY):
     if filename.endswith('.tar.gz'):
         # Extract the .tar.gz file
-        with tarfile.open(filename) as tar:
-            # Find the .tex file in the archive
-            tex_file = None
-            for member in tar.getmembers():
-                if member.name.endswith('.tex'):
-                    tex_file = member
-                    break
-            # If a .tex file is found, extract the tikzpicture block
-            if tex_file is not None:
-                with tar.extractfile(tex_file) as tex:
-                    # Read the .tex file and find the tikzpicture block
-                    tikz = ''
-                    output_filename = filename.replace('.tar.gz', '.tex')
-                    in_tikz = False
-                    for line in tex:
-                        line = line.decode('utf-8')
-                        if '\\begin{tikzpicture}' in line:
-                            in_tikz = True
-                        if in_tikz:
-                            tikz += line
-                        if '\\end{tikzpicture}' in line:
-                            in_tikz = False
-                    with open(output_filename, 'w') as output:
-                        output.write(tikz)
-                    print('Extracted TikZ code from ' + filename + ' to ' + output_filename)
-            else:
-                print('No .tex file found in ' + filename)        
+        output_filename = filename.replace('.tar.gz', '.tex')
+        if output_filename in os.listdir(OUTPUT_DIRECTORY):
+            print("found " + output_filename)
+            continue
+
+        try:
+            with tarfile.open(SOURCE_DIRECTORY / filename) as tar:
+                # Find the .tex file in the archive
+                tex_file = None
+                for member in tar.getmembers():
+                    if member.name.endswith('.tex'):
+                        tex_file = member
+                        break
+
+                # If a .tex file is found, extract the tikzpicture block
+                if tex_file is not None:
+                    with tar.extractfile(tex_file) as tex:
+                        # Read the .tex file and find the tikzpicture block
+                        tikz = ''
+                        in_tikz = False
+                        for line in tex:
+                            line = line.decode('utf-8')
+                            if '\\begin{tikzpicture}' in line:
+                                in_tikz = True
+                            if in_tikz:
+                                tikz += line
+                            if '\\end{tikzpicture}' in line:
+                                in_tikz = False
+                        if tikz != '':
+                            with open(OUTPUT_DIRECTORY / output_filename, 'w') as output:
+                                output.write(tikz)
+                            print(filename + ': extracted TikZ code to ' + output_filename)
+                        else:
+                            print(filename + ': no TikZ found')
+                else:
+                    print(filename + ': no .tex file found')   
+        except:
+           print(filename + ': not found or corrupted, skipping')
